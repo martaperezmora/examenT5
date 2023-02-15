9951c4215ea5e7662c26c765885a250ee150726b
Marta Perez <mpermor2205@g.educaand.es>
2023-02-15 10:50:20 +0100

    añadidas anotaciones en archivo seguridad

--- src/main/java/com/oretania/portal/configuration/SecurityConfig.java
+++ src/main/java/com/oretania/portal/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,7 +22,6 @@ public class SecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
         // return NoOpPasswordEncoder.getInstance();
     }
-
 
     @Bean
     public UserDetailsService users() {
@@ -49,7 +50,7 @@ public class SecurityConfig {
         return authProvider;
     }
 
-    // @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
