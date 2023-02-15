bf367e09c65892f789e6cb7b731e23ab1aace6ce
Marta Perez <mpermor2205@g.educaand.es>
2023-02-15 12:05:07 +0100

    editado controladores

--- src/main/java/com/oretania/portal/controllers/LoginController.java
+++ src/main/java/com/oretania/portal/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package com.oretania.portal.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,4 +18,22 @@ public class LoginController {
 
         return "welcome";
     }
+
+    // ejemplos controladores para las asignaturas
+    // con rutas protegidas
+    @GetMapping(value = { "/asignaturas/matematicas" })
+    @PreAuthorize("hasAnyAuthority('DIC','MAT')")
+    public String mat() {
+
+        return "asignaturas/matematicas";
+    }
+
+    @GetMapping(value = { "/asignaturas/lengua" })
+    @PreAuthorize("hasAnyAuthority('DIC','LEN')")
+    public String len() {
+
+        return "asignaturas/lengua";
+    }
+
+
 }
